23a8a2ab717058883f0bf27dd579c6fee63d2c91
luanacdev <luana.cam20@gmail.com>
2023-08-01 22:22:44 -0300

    fix: create sessao

--- FilmesApi/Models/Endereco.cs
+++ FilmesApi/Models/Endereco.cs
@@ -7,8 +7,11 @@ namespace FilmesApi.Models
         [Key]
         [Required]
         public int Id { get; set; }
+
         public string Logradouro { get; set; }
+
         public int Numero { get; set; }
+
         public virtual Cinema Cinema { get; set; }
     }
 }
